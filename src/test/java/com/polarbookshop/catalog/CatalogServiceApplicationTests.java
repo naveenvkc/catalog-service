e294df5464f193f22743b369eb63100a8fd4f0a8
Naveen Vasireddy <naveen.vkc@gmail.com>
2025-04-03 23:21:26 +0530

    publish container image

--- src/test/java/com/polarbookshop/catalog/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog/CatalogServiceApplicationTests.java
@@ -7,7 +7,9 @@ import com.polarbookshop.catalog.model.consumer.rest.BookResponseModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -15,45 +17,45 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
-//	@Autowired
-//	private WebTestClient webTestClient;
-
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient webTestClient;
 
 //	@Test
-//	void whenPostRequestThenBookCreated() {
-////		var expectedBook = BookRequestModel.of("1231231231", "Title", "Author", new BigDecimal("9.90"));
-////
-////		/*BookResponseModel.builder()
-////				.isbn("12312312")
-////				.title("Title")
-////				.author("Author")
-////				.price(new BigDecimal("9.90"))
-////				.build();*/
-////
-////		Map<String, String> headerParams = new HashMap<>();
-////		headerParams.put(Constants.X_B3_TRACEID, "43243");
-////		headerParams.put(Constants.X_B3_SPANID, "34343245");
-////
-////		webTestClient
-////				.post()
-////				.uri("/api/v1/books")
-////				.headers(headers -> {
-////					headers.setAll(headerParams);
-////				})
-////				.bodyValue(expectedBook)
-////				.exchange()
-////				.expectStatus().is2xxSuccessful()
-////				.expectBody(AddBookResponseModel.class).value(actualBook -> {
-////					assertThat(actualBook).isNotNull();
-////					assertThat(actualBook.getData().getIsbn()).isEqualTo(expectedBook.getIsbn());
-////				});
+//	void contextLoads() {
 //	}
+
+	@Test
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = BookRequestModel.of("1231231231", "Title", "Author", new BigDecimal("9.90"));
+
+		/*BookResponseModel.builder()
+				.isbn("12312312")
+				.title("Title")
+				.author("Author")				.price(new BigDecimal("9.90"))
+				.build();*/
+
+		Map<String, String> headerParams = new HashMap<>();
+		headerParams.put(Constants.X_B3_TRACEID, "43243");
+		headerParams.put(Constants.X_B3_SPANID, "34343245");
+
+		webTestClient
+				.post()
+				.uri("/api/v1/books")
+				.headers(headers -> {
+					headers.setAll(headerParams);
+				})
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(AddBookResponseModel.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.getData().getIsbn()).isEqualTo(expectedBook.getIsbn());
+				});
+	}
 
 }
